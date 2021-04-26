353f611f357eb7522731003068f7e9953f74e888
Alex Speranza <alex.speranza@studio.unibo.it>
2021-04-26 12:29:15 +0200

    Adds java test

---
+++ src/test/java/SperanzaTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SperanzaTest {
+    @Test
+    public void testJavaClass() {
+        assertEquals(Speranza.getOne(), 1);
+    }
+}
