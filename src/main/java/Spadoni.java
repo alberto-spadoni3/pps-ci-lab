72995713e9606b347fb95e4db85ff734a7ac7092
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2021-04-27 11:36:45 +0200

    Step 4

---
+++ src/main/java/Spadoni.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class Spadoni {
+    public static void main(String[] args) {
+        var random = new Random();
+        System.out.println(random.nextDouble());
+    }
+}
